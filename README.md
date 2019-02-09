4681dc20e479df458b07c2c12ea34d3b841eeddc
ndb796 <ndb796@naver.com>
2019-02-10 04:18:51 +0900

    Create React Project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Management System
+본 프로젝트는 React 강의 목적으로 만들어진 고객 관리 시스템(Management System)입니다.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>Let's develop management system!</h2>
       </div>
     );
   }
