5b452426e70b9ec6fbdc9bd936c1107a09b04157
ndb796 <ndb796@naver.com>
2019-02-10 16:26:46 +0900

    Add Node.js Express Server Module

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/api/hello', (req, res) => {
+    res.send({message: 'Hello Express!'});
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
