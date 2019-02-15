f8635507c0068833bfe96ac17635e53305823f7d
ndb796 <ndb796@naver.com>
2019-02-16 08:04:31 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => {
-              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/> );
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
             }) :
             <TableRow>
               <TableCell colSpan="6" align="center">
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/1',
-          'name': '나동빈',
-          'birthday': '961222',
-          'gender': '남자',
-          'job': '대학생'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/2',
-          'name': '홍길동',
-          'birthday': '960305',
-          'gender': '남자',
-          'job': '프로그래머'
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/64/3',
-          'name': '이순신',
-          'birthday': '921205',
-          'gender': '남자',
-          'job': '디자이너'
-        }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+          res.send(rows);
+      }  
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
